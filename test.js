e7e73907718325e68280dc2485296d00595b50c3
shanyemangfu <rmb8848@live.cn>
2020-04-16 11:43:48 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+<script>
+
+var x, y, z;  // 语句 1
+x = 22;        // 语句 2
+y = 11;        // 语句 3
+z = x + y;    // 语句 4
+
+document.getElementById("demo").innerHTML =
+"z 的值是" + z + "。";  
+
+</script>
